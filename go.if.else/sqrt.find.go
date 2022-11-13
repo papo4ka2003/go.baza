a84793c3b1332c41fa2a9be71c69775126e9f54f
tagilla0 <tagilla@list.ru>
2022-11-13 23:18:53 +0300

    Исправил ошибку связанную с расстановкой приоритета операторов и сделал вывод значений до четырех знаков после запятой

--- go.if.else/sqrt.find.go
+++ go.if.else/sqrt.find.go
@@ -24,18 +24,19 @@ func main() {
 		var x1 float64
 		var x2 float64
 
-		x1 = (-b + math.Sqrt(D)) / 2 * a
-		x2 = (-b - math.Sqrt(D)) / 2 * a
+		x1 = (-b + math.Sqrt(D)) / (2 * a)
+		x2 = (-b - math.Sqrt(D)) / (2 * a)
 
-		fmt.Println("Уравнение имеет 2 корня: " + fmt.Sprint(x1) + " и " + fmt.Sprint(x2))
+		fmt.Println("Уравнение имеет 2 корня: " + fmt.Sprintf("%.4f", x1) + " и " + fmt.Sprintf("%.4f", x2))
 	} else if D == 0 {
-		var x1 float64
+		var x float64
 
-		x1 = (-b - math.Sqrt(D)) / 2 * a
+		x = (-b) / (2 * a)
 
-		fmt.Println("Уравнение имеет 1 корень: " + fmt.Sprint(x1))
+		fmt.Print("Уравнение имеет 1 корень: " + fmt.Sprintf("%.4f", x))
 	} else {
 		fmt.Println("Уравнение не имеет корней")
 	}
 
+	fmt.Scan(&a)
 }
